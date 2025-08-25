d4976d3a5e96c7d8a9dd9de2fd372fc5da20489f
ivona <ik87056@student.uwb.edu.pl>
2025-08-25 18:59:52 +0200

    added doGet method

--- src/main/java/com/servlets/RemoteRequestServlet.java
+++ src/main/java/com/servlets/RemoteRequestServlet.java
@@ -8,4 +8,8 @@ import java.net.URL;
 
 public class RemoteRequestServlet extends HttpServlet {
 
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
 }
