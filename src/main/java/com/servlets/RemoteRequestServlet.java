8a92e2361be12eecc93d444313e53d64e050cb7a
ivona <ik87056@student.uwb.edu.pl>
2025-08-25 20:28:19 +0200

    getting parameter urln from a request

--- src/main/java/com/servlets/RemoteRequestServlet.java
+++ src/main/java/com/servlets/RemoteRequestServlet.java
@@ -11,5 +11,14 @@ public class RemoteRequestServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        String urln_param = request.getParameter("urln");
+
+        if (urln_param == null || urln_param.isEmpty()) {
+            out.println("<h2>Parameter 'urln' is empty.</h2>");
+            return;
+        }
     }
 }
