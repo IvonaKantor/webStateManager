df764d43fc003caf247f2d6cad38634165d7c863
ivona <ik87056@student.uwb.edu.pl>
2025-08-25 18:55:37 +0200

    added a servlet for remote requests

---
+++ src/main/java/com/servlets/RemoteRequestServlet.java
@@ -0,0 +1,11 @@
+package com.servlets;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RemoteRequestServlet extends HttpServlet {
+
+}
