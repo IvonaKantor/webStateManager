0ba2f613c6a345ddf7873fd275a15323147e6d96
ivona <ik87056@student.uwb.edu.pl>
2025-08-27 23:55:06 +0200

    update

--- src/main/java/com/servlets/GetURLServlet.java
+++ src/main/java/com/servlets/GetURLServlet.java
@@ -6,14 +6,13 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class RemoteRequestServlet extends HttpServlet {
+@WebServlet("/GetURL")
+public class GetURLServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-
         String urln_param = request.getParameter("urln");
 
         if (urln_param == null || urln_param.isEmpty()) {
